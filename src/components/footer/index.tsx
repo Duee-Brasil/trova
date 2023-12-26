b5328617056f9586d59f293fcf516b1036366406
RubensMatos <rubens.matos.filho@gmail.com>
2023-12-26 17:56:26 -0300

    Atualização - footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -34,16 +34,13 @@ const Paragraph = styled.p`
 const Footer = () => {
 
   useEffect(() => {
-    // Criar um elemento script
+
     const scriptElement = document.createElement('script');
 
-    // Configurar o conteúdo do script (pode ser um URL de script externo ou o próprio código)
-    scriptElement.textContent = "const todasAsImagens = document.querySelectorAll('img');  todasAsImagens.forEach((imagem) => {  imagem.style.width = '300px';  imagem.style.height = '200px'; });";
+    scriptElement.textContent = " const todasAsImagens = document.querySelectorAll('img'); todasAsImagens.forEach((imagem) => { const largura = imagem.clientWidth; const altura = imagem.clientHeight; imagem.setAttribute('width', largura.toString()); imagem.setAttribute('height', altura.toString()); }); ";
 
-    // Adicionar o script ao final do corpo do documento
     document.body.appendChild(scriptElement);
 
-    // Limpar o script quando o componente for desmontado
     return () => {
       document.body.removeChild(scriptElement);
     };
