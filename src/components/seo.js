062ceb8b3fce5d6b105592579b213caedc2b438c
RubensMatos <rubens.matos.filho@gmail.com>
2023-12-27 16:21:37 -0300

    Atualização - seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -20,7 +20,7 @@ function Seo({ title }) {
   const metaDescription = site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
 
-  return (
+  return (    
     <Helmet
       htmlAttributes={{
         lang: "pt-br"
@@ -62,62 +62,38 @@ function Seo({ title }) {
         },
       ]}
     >
-        <link rel="dns-prefetch" href="https://gstatic.com" crossorigin />
-        <link rel="dns-prefetch" href="https://googleapis.com" crossorigin />
-        
-        <link rel="dns-prefetch" href="googletagmanager.com" crossorigin /> 
-        <link rel="dns-prefetch" href="google-analytics.com" crossorigin />
-        <link rel="dns-prefetch" href="unpkg.com" crossorigin />
-        
-        <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="dns-prefetch" href="https://gstatic.com" crossorigin />
+      <link rel="dns-prefetch" href="https://googleapis.com" crossorigin />
 
-        <link rel="preconnect" href="www.googletagmanager.com"  crossorigin />  
-        <link rel="preconnect" href="www.google-analytics.com"  crossorigin />
-        <link rel="preconnect" href="unpkg.com"  crossorigin />
+      <link rel="dns-prefetch" href="googletagmanager.com" crossorigin />
+      <link rel="dns-prefetch" href="google-analytics.com" crossorigin />
+      <link rel="dns-prefetch" href="unpkg.com" crossorigin />
 
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
-          rel="stylesheet"
-          as="style"
-          type="text/css"
-          media="all"
-        />
+      <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 
-<script type="text-javascript"> {`
-   
-    function loadFontAsync() {
-      var link = document.createElement('link');
-      link.href = 'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap';
-      link.rel = 'stylesheet';
-      link.as = 'style';
-      link.type = 'text/css';
-      link.media = 'all';
-      document.head.appendChild(link);
-    }
+      <link rel="preconnect" href="www.googletagmanager.com" crossorigin />
+      <link rel="preconnect" href="www.google-analytics.com" crossorigin />
+      <link rel="preconnect" href="unpkg.com" crossorigin />
 
-    window.addEventListener('load', loadFontAsync);
-    `}
-    </script>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+        rel="stylesheet"
+        as="style"
+        type="text/css"
+        media="all"
+      />
+      <link rel="canonical" href="https://trovabrasil.com/" />
+      <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js" as="script"
+        type="text/javascript"
+        async></script>
 
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/swiper@8/swiper-bundle.min.css"
-          as="style"
-          type="text/css"
-          media="all"
-        />
-        <link rel="canonical" href="https://trovabrasil.com/" />
-        <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js" as="script"
-          type="text/javascript"
-          async></script>
-
-        <meta
-          name="facebook-domain-verification"
-          content="za65m80ibiardiepk5vq60o4m8sw2l"
-        />
-        <script type="application/ld+json">
-          {`{
+      <meta
+        name="facebook-domain-verification"
+        content="za65m80ibiardiepk5vq60o4m8sw2l"
+      />
+      <script type="application/ld+json">
+        {`{
           "@context": "http://schema.org",
           "@type": "Organization",
           name: "Trova Brasil",
@@ -129,12 +105,19 @@ function Seo({ title }) {
             "https://br.pinterest.com/trovatravertino/_created/",
           ],
         }`}
-        </script>
-        <meta name="description" content={metaDescription}></meta>
-        <meta name="copyright" content="@letgodoy"></meta>
-        <meta name="url" content="https://trovabrasil.com"></meta>
-        <meta name="identifier-URL" content="https://trovabrasil.com"></meta>
-        <meta http-equiv="Cache-Control" content="public, max-age=0, must-revalidate"></meta>
+      </script>
+      <meta name="description" content={metaDescription}></meta>
+      <meta name="copyright" content="@letgodoy"></meta>
+      <meta name="url" content="https://trovabrasil.com"></meta>
+      <meta name="identifier-URL" content="https://trovabrasil.com"></meta>
+      <meta http-equiv="Cache-Control" content="public, max-age=0, must-revalidate"></meta>
+
+      <style>
+        {`
+          @import url('https://unpkg.com/swiper@8/swiper-bundle.min.css');
+        `}
+      </style>
+
     </Helmet>
   )
 }
